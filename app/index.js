d0e42b5a5f7a4f0837353055f87b7ce858b93e00
kornalius <arianesoftinc@gmail.com>
2017-11-14 15:48:25 -0500

    new pointers

--- app/index.js
+++ app/index.js
@@ -2,4 +2,6 @@ const utils = require('./utils')
 
 window.Repress = {
   ItemsMixin: window.Polymer.dedupingMixin(require('./mixins/items')),
+  StatsMixin: window.Polymer.dedupingMixin(require('./mixins/stats')),
+  DisabledMixin: window.Polymer.dedupingMixin(require('./mixins/disabled')),
 }
